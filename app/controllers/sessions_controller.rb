33f16fa871f5c530ef18a15f9bc0a72685e355e7
AhmedCommando5198 <33036151+AhmedCommando5198@users.noreply.github.com>
2018-03-31 18:22:00 +0200

    login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,11 @@ class SessionsController < ApplicationController
       if user && user.authenticate(params[:session][:password])
         # Log the user in and redirect to the user's show page.
       else
-        # Create an error message.
+         # Create an error message.
         render 'new'
       end
     end
+
 
   def destroy
   end
