b5631d9687a2251e41dc3f8a6324fce8ca1b4529
AhmedCommando5198 <33036151+AhmedCommando5198@users.noreply.github.com>
2018-04-06 20:01:36 +0200

    step back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_filter :check_token
-  protected
-    def check_token
-      unless session[:token]
-        session[:original_url] = request.url
-        redirect_to :controller => :logins, :action => :new
-      end
-    end
-  end
+zend
